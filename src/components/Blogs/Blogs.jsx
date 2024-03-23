d6406a6d39708e307ceac02ed195081b56957d99
ferdousahammed <ferdousahamedrimon2@gmail.com>
2024-03-23 14:49:50 +0600

    Layout and Navbar Added

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,9 @@
+const Blogs = () => {
+  return (
+    <div className="">
+      <h2>Blogs</h2>
+    </div>
+  );
+};
+
+export default Blogs;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Jobs from "./components/Jobs/Jobs.jsx";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs.jsx";
+import Blogs from "./components/Blogs/Blogs.jsx";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    errorElement: <ErrorPage />,
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+    ],
   },
 ]);
 
